4bf778ab6abff9819a26becf3745850a5d0d1005
JQ <JQ@janices-mbp.attlocal.net>
2022-05-01 21:55:05 -0700

    troubleshoot fix

--- static/js/charts.js
+++ static/js/charts.js
@@ -59,38 +59,105 @@ function buildCharts(sample) {
   d3.json("./static/data/samples.json").then((data) => {
     // 3. Create a variable that holds the samples array. 
     var samplesArray = data.samples;
-    console.log(samplesArray)
+    // console.log(samplesArray)
     // 4. Create a variable that filters the samples for the object with the desired sample number.
-    var desiredSample = samplesArray.filter(data => data.id == sample);
-    console.log(desiredSample);
+    var desiredSample = samplesArray.filter(obj => obj.id == sample);
+    // console.log(desiredSample);
+
+    // 3-1. Create a variable that filters the metadata array for the object with the desired sample number.
+    var metadata = data.metadata;
+    var metadataArray = metadata.filter(sampleObj => sampleObj.id == sample);
 
     //  5. Create a variable that holds the first sample in the array.
     var firstSample = desiredSample[0];
-    console.log(firstSample);
+    // console.log(firstSample);
+
+    // 3-2. Create a variable that holds the first sample in the metadata array.
+    var metaResult = metadataArray[0];
+    // console.log(metaResult);   
 
     // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
     var otuIds = firstSample.otu_ids;
     var otuLabels = firstSample.otu_labels;
     var sampleValues = firstSample.sample_values;
-    console.log(otuIds);
-    console.log(otuLabels);
-    console.log(sampleValues);
+    // console.log(otuIds);
+    // console.log(otuLabels);
+    // console.log(sampleValues);
+
+    // 3-3. Create a variable that holds the washing frequency.
+    var washingFreq = parseInt(metaResult.wfreq);      
 
     // 7. Create the yticks for the bar chart.
     // Hint: Get the the top 10 otu_ids and map them in descending order  
     //  so the otu_ids with the most bacteria are last. 
-    var out_idsSort = out_ids.sort((a,b) => a - b).reverse(); 
-    var yticks = out_idsSort.slice(0,10)
+    var otuIdsSort = otuIds.sort((a,b) => a - b).reverse(); 
+    var yticks = otuIdsSort.slice(0,10);
+    var xticks = sampleValues.slice(0,10).reverse();
 
     // 8. Create the trace for the bar chart. 
-    var barData = [
-      
-    ];
+    var barData = [{
+      x: xticks,
+      y: yticks,
+      type: "bar",
+      orientation: "h",
+      text: otuLabels
+    }];
+
     // 9. Create the layout for the bar chart. 
     var barLayout = {
-     
+      title: "Top 10 OTU"     
     };
     // 10. Use Plotly to plot the data with the layout. 
+    Plotly.newPlot("bar", barData, barLayout);
+
+    // 2-1. Create the trace for the bubble chart.
+    var bubbleData = [{
+      x: otuIds,
+      y: sampleValues,
+      text: otuLabels,
+      mode: "markers",
+      marker: {
+        size: sampleValues,
+        color: otuIds
+      }
+    }];
+
+    // 2-2. Create the layout for the bubble chart.
+    var bubbleLayout = {
+      title: "Bacteria Cultures Per Sample",
+      xaxis: {title: "xx"},
+    };
+
+    // 2-3. Use Plotly to plot the data with the layout.
+    Plotly.newPlot("bubble", bubbleData, bubbleLayout);
     
+    
+    // 3-4. Create the trace for the gauge chart.
+    var gaugeData = [{
+      value: washingFreq,
+      title: {text: "Belly Button Washing Frequency<br>Scrubs per Week"},
+      type: "indicator",
+      mode: "gauge+number",
+      gauge: {
+        axis: {range: [0,10]},
+        steps: [
+          {range: [0,2], color:"#ea2c2c"},
+          {range: [2,4], color:"#ea822c"},
+          {range: [4,6], color:"#ee9c00"},
+          {range: [6,8], color:"#eecc00"},
+          {range: [8,10], color:"#d4ee00"}
+        ]
+      }
+    }];
+    
+    // 3-5. Create the layout for the gauge chart.
+    var gaugeLayout = { 
+      width: 600, height: 450, margin: {t: 0, b: 0}     
+    };
+
+    // 3-6. Use Plotly to plot the gauge data and layout.
+    Plotly.newPlot("gauge", gaugeData, gaugeLayout);
+
+
   });
 }
